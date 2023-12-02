9a4ccceaebc13b57c64328e48fc8fd45e06dc601
Luciano Martins <zicadopv@gmail.com>
2023-12-02 17:39:53 -0300

    fix build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "BUILD START"
-python3.9 -m pip install -q -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 echo "BUILD END"
