0400e0cf3837d73280b10b128a9f8caa871514e2
Luciano Martins <zicadopv@gmail.com>
2024-01-09 08:04:35 -0300

    Delete libs: pyitau and djangorestframework

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 echo "Building project packages..."
+python3 -m pip install --upgrade pip
+
+echo "Building project packages..."
 python3 -m pip install -q -r requirements.txt
 
 echo "Migrating Database..."
